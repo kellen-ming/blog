28e741a944326a24328c619d6e186a086ebb478c
梁志明 <liangzhiming@yhmsi.com>
2025-05-15 20:31:38 +0800

    update

--- src/i18n/languages/zh_CN.ts
+++ src/i18n/languages/zh_CN.ts
@@ -3,7 +3,8 @@ import type { Translation } from '../translation'
 
 export const zh_CN: Translation = {
   [Key.home]: '主页',
-  [Key.about]: '关于',
+  // [Key.about]: '关于',
+  [Key.about]: '练习',
   [Key.archive]: '归档',
   [Key.search]: '搜索',
 
